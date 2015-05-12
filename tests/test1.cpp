be94d298b507d4b8f2f7c2596dc20089cc7d6d1b
Staroselskii Georgii <gosha371@gmail.com>
2015-05-12 16:55:53 +0300

    first tests are in place

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,6 +2,23 @@
 
 #include <drivers/NavioRCIO.h>
 
-TEST(IndependentMethod, ResetsToZero) {
-	EXPECT_EQ(0,1);
+class NavioRCIO_Test : public ::testing::Test {
+protected:
+
+	NavioRCIO_Test() {
+        rcio.init();
+	}
+
+	virtual ~NavioRCIO_Test() {
+	}
+
+    NavioRCIO rcio{};
+};
+
+TEST_F(NavioRCIO_Test, Init) {
+    EXPECT_EQ(rcio.init(), true);
+}
+
+TEST_F(NavioRCIO_Test, Detection) {
+    EXPECT_EQ(rcio.detect(), true);
 }
