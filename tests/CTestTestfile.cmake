2521fe43856b10e302ec4a1a70ab7580d9e7156e
Staroselskii Georgii <gosha371@gmail.com>
2015-07-09 19:52:16 +0300

    cmake: removed CTestTestfile.cmake

--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/george/Documents/code/emlid/navio/stm32/navio-rcio-linux-driver/tests
-# Build directory: /home/george/Documents/code/emlid/navio/stm32/navio-rcio-linux-driver/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(RCIO_Test "/home/george/Documents/code/emlid/navio/stm32/navio-rcio-linux-driver/tests/rcio_test")
